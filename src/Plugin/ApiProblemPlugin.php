3d6b05fe999d7ae589032fba9a8f619c5d9c33e3
sam <gandalf.blanc@yahoo.fr>
2024-04-06 22:28:57 +0200

    api problem plugin

--- src/Plugin/ApiProblemPlugin.php
+++ src/Plugin/ApiProblemPlugin.php
@@ -3,12 +3,16 @@
 namespace SamuelPouzet\Api\Plugin;
 
 use Laminas\Mvc\Controller\Plugin\AbstractPlugin;
+use Laminas\View\Model\JsonModel;
 
 class ApiProblemPlugin extends AbstractPlugin
 {
-    public function __invoke()
+    public function __invoke(int $statusCode, string $message)
     {
-        die('api problem test');
-        //$this->getController()->redirect()->toRoute();
+        $this->getController()->getResponse()->setStatusCode($statusCode);
+        return new JsonModel([
+            'status' => $statusCode,
+            'message' => $message
+        ]);
     }
 }
